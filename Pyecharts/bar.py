0fcdb7e330cf3285786ce26a7d9d2e7939b04970
yanglb <yanglb@fxtcn.com>
2019-01-30 18:13:47 +0800

    --

--- Pyecharts/bar.py
+++ Pyecharts/bar.py
@@ -14,9 +14,15 @@
 """
 from pyecharts import Bar
 
-bar = Bar("我的第一个图表", "这里是副标题")
-bar.use_theme('dark')
-bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
-# bar.print_echarts_options() # 该行只为了打印配置项，方便调试时使用
-bar.render()    # 生成本地 HTML 文件
-bar.render(path='snapshot.png')
+
+def iniBar():
+    bar = Bar("我的第一个图表", "这里是副标题")
+    bar.use_theme('dark')
+    bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
+    # bar.print_echarts_options() # 该行只为了打印配置项，方便调试时使用
+    bar.render()  # 生成本地 HTML 文件
+    bar.render(path='snapshot.png')
+
+
+def __main__():
+    iniBar()
